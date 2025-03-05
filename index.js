472ed3ee50137108b17ed76a0a3a80205d55a055
Christian Grimm <grimmchristian@gmx.de>
2025-03-05 20:25:22 +0100

    Add functions

--- index.js
+++ index.js
@@ -10,20 +10,101 @@ const questions = [
         correct: false,
       },
       {
-        id: "a",
-        text: "München",
+        id: "b",
+        text: "Berlin",
+        correct: true,
+      },
+      {
+        id: "c",
+        text: "Hamburg",
         correct: false,
       },
+      {
+        id: "d",
+        text: "Hannover",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "Was ist die Hauptstadt von Frankreich",
+    answers: [
       {
         id: "a",
         text: "München",
         correct: false,
       },
       {
-        id: "a",
-        text: "München",
+        id: "b",
+        text: "Berlin",
+        correct: true,
+      },
+      {
+        id: "c",
+        text: "Hamburg",
+        correct: false,
+      },
+      {
+        id: "d",
+        text: "Hannover",
         correct: false,
       },
     ],
   },
 ];
+
+let currentQuestion;
+let currentQuestionPointer = -1;
+
+function renderQuestion(question) {
+  const questionDiv = document.createElement("div");
+  questionDiv.id = question.id;
+  questionDiv.classList.add("question");
+
+  const questionTitle = document.createElement("div");
+  questionDiv.classList.add("question-title");
+
+  questionTitle.appendChild(document.createTextNode(question.question));
+
+  const questionAnswers = document.createElement("div");
+  questionAnswers.classList.add("question-answers");
+
+  question.answers.forEach((answer) => {
+    const answerDiv = document.createElement("button");
+    answerDiv.id = answer.id;
+    answerDiv.onclick = () => {
+      sendAnswer(answer);
+    };
+    answerDiv.classList.add("answer");
+    answerDiv.appendChild(document.createTextNode(answer.text));
+    questionAnswers.appendChild(answerDiv);
+  });
+
+  questionDiv.appendChild(questionTitle);
+  questionDiv.appendChild(questionAnswers);
+  document.getElementById("display-question").appendChild(questionDiv);
+}
+
+function nextQuestion() {
+  if (currentQuestion) {
+    document.getElementById(String(currentQuestion.id)).remove();
+  }
+
+  if (currentQuestionPointer + 1 < questions.length) {
+    currentQuestionPointer++;
+    currentQuestion = questions[currentQuestionPointer];
+  } else {
+    currentQuestionPointer = 0;
+    currentQuestion = questions[currentQuestionPointer];
+  }
+  renderQuestion(currentQuestion);
+}
+
+function sendAnswer(answer) {
+  console.log(`Antowort ist ${answer.correct}, Answer ID ist ${answer.id}`);
+  answer.correct ? console.log("Grün") : console.log("Rot");
+  currentQuestion.answers.forEach((ans) => {
+    console.log(answer.correct, answer.id, ans.correct, ans.id);
+  });
+}
