ced5dbf4ea0f7f022cadf8b065965fd35fcd767f
Christian Grimm <grimmchristian@gmx.de>
2025-03-05 18:01:17 +0100

    Add the object-structure for questions

---
+++ index.js
@@ -0,0 +1,29 @@
+// TODO 10 Fragenstruktur
+const questions = [
+  {
+    id: 1,
+    question: "Was ist die Hauptstadt von Deutschland",
+    answers: [
+      {
+        id: "a",
+        text: "München",
+        correct: false,
+      },
+      {
+        id: "a",
+        text: "München",
+        correct: false,
+      },
+      {
+        id: "a",
+        text: "München",
+        correct: false,
+      },
+      {
+        id: "a",
+        text: "München",
+        correct: false,
+      },
+    ],
+  },
+];
